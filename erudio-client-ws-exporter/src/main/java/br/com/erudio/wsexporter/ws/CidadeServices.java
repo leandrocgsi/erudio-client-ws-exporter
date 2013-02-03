aaf9e878ac2e59251ab4ee6b9614c12235ed8480
Leandro da Costa Gonçalves <leandrocgsi@gmail.com>
2013-02-02 23:56:34 -0200

    Adição de suporte ao SpringSecurity

---
+++ erudio-client-ws-exporter/src/main/java/br/com/erudio/wsexporter/ws/CidadeServices.java
@@ -0,0 +1,47 @@
+package br.com.erudio.wsexporter.ws;
+
+import br.com.erudio.utils.database.beans.BeanCidade;
+import br.com.erudio.utils.service.interfaces.ICidadeUtilServices;
+import br.com.erudio.utils.service.ws.interfaces.ICidadeRestService;
+import br.com.erudio.wsexporter.factory.Factory;
+import java.util.List;
+import javax.inject.Named;
+import org.apache.log4j.Logger;
+
+
+@Named
+public class CidadeServices implements ICidadeRestService {
+
+    private Logger logger = Logger.getLogger(this.getClass());
+    
+    private static ICidadeUtilServices serviceCidade = Factory.getServiceCidade();    
+
+    public CidadeServices() {
+        this.logger.info("Acessando o web-service de cidade.");
+    }
+
+    @Override
+    public Integer saveCidade(BeanCidade cidade) {
+        return serviceCidade.saveCidade(cidade);
+    }
+
+    @Override
+    public void updateCidade(BeanCidade cidade) {
+        serviceCidade.updateCidade(cidade);
+    }
+
+    @Override
+    public void deleteCidade(BeanCidade cidade) {
+        serviceCidade.deleteCidade(cidade);
+    }
+    
+    @Override
+    public List<BeanCidade> findAllCidades(){ 
+        return serviceCidade.findAllCidades();
+    }
+
+    @Override
+    public BeanCidade findCidadeById(Integer id) {
+        return serviceCidade.findCidadeById(id);
+    }    
+}
